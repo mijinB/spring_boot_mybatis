d8f1d2e257adf86639d88d1c21412556b4b1abc9
mijinB <bmj13465@gmail.com>
2024-11-06 22:24:32 +0900

    게시글 작성 화면 생성

--- src/main/java/com/codingrecipe/board/controller/BoardController.java
+++ src/main/java/com/codingrecipe/board/controller/BoardController.java
@@ -3,10 +3,16 @@ package com.codingrecipe.board.controller;
 import com.codingrecipe.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @RequiredArgsConstructor
 public class BoardController {
 
     private final BoardService boardService;
+
+    @GetMapping("/save")        // url 이름
+    public String save() {      // java method 이름
+        return "save";          // return 할 화면의 이름
+    }
 }
