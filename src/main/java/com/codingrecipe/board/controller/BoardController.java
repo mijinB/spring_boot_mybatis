bf34db5e91452c70d48a7903cbd56b587e7a796e
mijinB <bmj13465@gmail.com>
2024-11-12 21:21:33 +0900

    게시글 작성 데이터 전송

--- src/main/java/com/codingrecipe/board/controller/BoardController.java
+++ src/main/java/com/codingrecipe/board/controller/BoardController.java
@@ -1,9 +1,12 @@
 package com.codingrecipe.board.controller;
 
+import com.codingrecipe.board.dto.BoardDTO;
 import com.codingrecipe.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequiredArgsConstructor
@@ -14,5 +17,12 @@ public class BoardController {
     @GetMapping("/save")        // url 이름
     public String save() {      // java method 이름
         return "save";          // return 할 화면의 이름
+    }
+
+    @PostMapping("/save")
+    public String save(BoardDTO boardDTO) {
+//  public String save(@ModelAttribute BoardDTO boardDTO) {       @ModelAttribute 은 생략 가능해서 생략한 것
+        System.out.println("boardDTO = " + boardDTO);
+        return "index";
     }
 }
--- src/main/java/com/codingrecipe/board/dto/BoardDTO.java
+++ src/main/java/com/codingrecipe/board/dto/BoardDTO.java
@@ -8,6 +8,12 @@ import lombok.ToString;
 @Setter
 @ToString
 public class BoardDTO {
+    /**
+     * HTML 의 name 이 아래 필드와 동일한 이름이여야 한다.
+     *  => Controller 에서 잘 받기 위함
+     *      BoardController.java 의 PostMapping("/save") 에서 매개변수(BoardDTO boardDTO)를 받아서 (=HTML 에서 Post 한 data 가 boardDTO)
+     *      현재 BoardDTO 클래스의 필드 이름과 동일하고 setter 가 있다면 자동으로 값을 담아주는 작업을 Spring 이 중간에서 진행해준다.
+     */
     private Long id;
     private String boardWriter;
     private String boardPass;
